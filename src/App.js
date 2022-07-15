91582d09dbf1a9af2515197de3171d1a82fa5399
Yamila Ester <yamila_anzaudo@hotmail.com>
2022-07-15 11:14:07 -0300

    corrigiendo errores con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
+import NavBar from './Components/NavBar';
+import CartWidget from './Components/CartWidget';
+import ItemListContainer from './Components/ItemListContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css";
-import NavBar from './Components/NavBar';
-import CarWidget from './Components/CartWidget';
-import ItemListContainer from './Components/ItemListContainer';
+
 
 function App() {
     return (
         <><><><><div> className="App"</div>
+
             <NavBar /></>
-            <CarWidget /></>
-            <div className="carousel-item"></div></><ItemListContainer /></>
+            <CartWidget /></>
+            </><ItemListContainer /></>
 );
     }
 
--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -1,22 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const ItemListContainer = () => {
-    return(
-        <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://www.youtube.com/watch?v=Ie9jI_bzCec" className="d-block w-100" alt="..."></img>
-    </div>
-    <div className="carousel-item">
-      <img src="https://www.youtube.com/watch?v=Ie9jI_bzCec" className="d-block w-100" alt="..."></img>
-    </div>
-    <div className="carousel-item">
-      <img src="https://www.youtube.com/watch?v=Ie9jI_bzCec" className="d-block w-100" alt=""></img>
-    </div>
-  </div>
-</div>
-);
+function ItemListContainer() {
+  return (
+    <><div className="List">
+    </div><div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <video src="https://www.youtube.com/watch?v=Ie9jI_bzCec" className="d-block w-100" alt="..."></video>
+          </div>
+          <div className="carousel-item">
+            <video src="" className="d-block w-100" alt="..."></video>
+          </div>
+          <div className="carousel-item">
+            <video src="https://www.youtube.com/watch?v=Ie9jI_bzCec" className="d-block w-100" alt=""></video>
+          </div>
+        </div>
+      </div></>
+  );
 }
 
 export default ItemListContainer;
